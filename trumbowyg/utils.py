8f9b26178465a8ae5cbaca506f59a221c17bddf0
devops724-2 <65301369+devops724-2@users.noreply.github.com>
2020-10-11 12:03:03 +0400

    Update utils.py

--- trumbowyg/utils.py
+++ trumbowyg/utils.py
@@ -1,5 +1,5 @@
 import logging
-from django.utils import six
+
 from django.utils.text import slugify as django_slugify
 
 from trumbowyg import settings as _settings
@@ -11,8 +11,8 @@ def slugify(value):
     if _settings.TRANSLITERATE_FILENAME:
         try:
             from unidecode import unidecode
-            value = unidecode(six.text_type(value))
+            value = unidecode(value)
         except ImportError as e:
             logger.exception(e)
 
-    return django_slugify(six.text_type(value))
+    return django_slugify(value)
